6afd8b674298d118bd4a8c8f4aad9db89cf4ec9f
Colter Ulrich <cju5025@gmail.com>
2021-03-21 21:22:03 -0600

    First Commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+
+class Chorus_FlangerAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    Chorus_FlangerAudioProcessorEditor (Chorus_FlangerAudioProcessor&);
+    ~Chorus_FlangerAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Chorus_FlangerAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Chorus_FlangerAudioProcessorEditor)
+};
